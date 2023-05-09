69cf1e5c68ddf36af16a5af4db1d5a487e6c5133
Carsten Thue-Bludworth <carstentb@protonmail.com>
2023-05-09 15:32:45 -0400

    Got template app working

---
+++ mindmap/egui integrations.md
@@ -0,0 +1,4 @@
+The `egui` documentation isn't super clear about the differences in building for Wayland vs X11.
+
+Do include `cargo.lock` in version control, as this is an application. See [here](https://doc.rust-lang.org/cargo/faq.html#why-do-binaries-have-cargolock-in-version-control-but-not-libraries)
+
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+fn main() -> eframe::Result<()> {
+    // Log to stdout (if you run with `RUST_LOG=debug`).
+    tracing_subscriber::fmt::init();
+
+    let native_options = eframe::NativeOptions::default();
+    eframe::run_native(
+        "Iron Coder",
+        native_options,
+        Box::new(|cc| Box::new(iron_coder::TemplateApp::new(cc))),
+    )
 }

c7376f6dce405c0ed8610c3d3575242cbc45dd6a
Carsten Thue-Bludworth <carstentb@protonmail.com>
2023-06-06 10:52:56 +0900

    starting cache optimization

--- src/editor.rs
+++ src/editor.rs
@@ -35,9 +35,55 @@ impl Default for CodeEditor {
 
 impl CodeEditor {
 
-    // I would honestly prefer to implement the CodeEditor display
-    // via the Widget trait (see below -- commented out). But I was
-    // fighting the borrow checker too much. This seems to work.
+    fn highlight(&mut self, code: &str, language: &str) -> LayoutJob {
+
+        // Destructure, and do the highlighting
+        let CodeEditor { code, ps, ts } = self;
+        let _code = code.clone();
+        let text = _code.as_str();
+
+        let syntax = ps.find_syntax_by_extension(language).unwrap();
+
+        let mut h = HighlightLines::new(syntax, &ts.themes["base16-ocean.dark"]);
+
+        use egui::text::{LayoutSection, TextFormat};
+
+        let mut job = LayoutJob {
+            text: text.into(),
+            ..Default::default()
+        };
+
+        for line in LinesWithEndings::from(text) {
+            for (style, range) in h.highlight_line(line, &ps).ok().unwrap() {
+                let fg = style.foreground;
+                let text_color = egui::Color32::from_rgb(fg.r, fg.g, fg.b);
+                let italics = style.font_style.contains(FontStyle::ITALIC);
+                let underline = style.font_style.contains(FontStyle::ITALIC);
+                let underline = if underline {
+                    egui::Stroke::new(1.0, text_color)
+                } else {
+                    egui::Stroke::NONE
+                };
+                job.sections.push(LayoutSection {
+                    leading_space: 0.0,
+                    byte_range: as_byte_range(text, range),
+                    format: TextFormat {
+                        font_id: egui::FontId::monospace(12.0),
+                        color: text_color,
+                        italics,
+                        underline,
+                        ..Default::default()
+                    },
+                });
+            }
+        }
+
+        return job;
+    }
+
+    // I would prefer to implement the CodeEditor display
+    // via the Widget trait I think (see below -- commented out).
+    // But I was fighting the borrow checker too much. This seems to work.
 
     // TODO -- Lots of optimizations (and opportunities for benchmarking)
     // regarding the syntax highlighting, as well as error checking and 
@@ -54,60 +100,22 @@ impl CodeEditor {
         let _code = code.clone();
         let text = _code.as_str();
 
-        if let Some(syntax) = ps.find_syntax_by_extension("rs") {
+        let mut layouter = |ui: &egui::Ui, string: &str, _wrap_width: f32| {
+            let layout_job = highlight(ctx, text, "rs");
+            ui.fonts(|f| f.layout_job(layout_job))
+        };
 
-            let mut h = HighlightLines::new(syntax, &ts.themes["base16-ocean.dark"]);
+        ui.add(
+            egui::TextEdit::multiline(code)
+                .font(egui::TextStyle::Monospace) // for cursor height
+                .code_editor()
+                .desired_rows(10)
+                .lock_focus(true)
+                .desired_width(f32::INFINITY)
+                .frame(false)
+                .layouter(&mut layouter),
+        );
 
-            use egui::text::{LayoutSection, TextFormat};
-
-            let mut layouter = |ui: &egui::Ui, string: &str, _wrap_width: f32| {
-                let mut job = LayoutJob {
-                    text: text.into(),
-                    ..Default::default()
-                };
-        
-                for line in LinesWithEndings::from(text) {
-                    for (style, range) in h.highlight_line(line, ps).ok().unwrap() {
-                        let fg = style.foreground;
-                        let text_color = egui::Color32::from_rgb(fg.r, fg.g, fg.b);
-                        let italics = style.font_style.contains(FontStyle::ITALIC);
-                        let underline = style.font_style.contains(FontStyle::ITALIC);
-                        let underline = if underline {
-                            egui::Stroke::new(1.0, text_color)
-                        } else {
-                            egui::Stroke::NONE
-                        };
-                        job.sections.push(LayoutSection {
-                            leading_space: 0.0,
-                            byte_range: as_byte_range(text, range),
-                            format: TextFormat {
-                                font_id: egui::FontId::monospace(12.0),
-                                color: text_color,
-                                italics,
-                                underline,
-                                ..Default::default()
-                            },
-                        });
-                    }
-                }
-                // layout_job.wrap.max_width = wrap_width; // no wrapping
-                ui.fonts(|f| f.layout_job(job))
-            };
-
-            ui.add(
-                egui::TextEdit::multiline(code)
-                    .font(egui::TextStyle::Monospace) // for cursor height
-                    .code_editor()
-                    .desired_rows(10)
-                    .lock_focus(true)
-                    .desired_width(f32::INFINITY)
-                    .frame(false)
-                    .layouter(&mut layouter),
-            );
-
-        } else {
-            return;
-        }
     }
 
 }
@@ -120,6 +128,25 @@ fn as_byte_range(whole: &str, range: &str) -> std::ops::Range<usize> {
     let offset = range_start - whole_start;
     offset..(offset + range.len())
 }
+
+
+pub fn highlight(ctx: &egui::Context, code: &str, language: &str) -> LayoutJob {
+    // Implement this trait for the CodeEditor struct
+    impl egui::util::cache::ComputerMut<(&str, &str), LayoutJob> for CodeEditor {
+        fn compute(&mut self, (code, lang): (&str, &str)) -> LayoutJob {
+            self.highlight(code, lang)
+        }
+    }
+
+    type HighlightCache = egui::util::cache::FrameCache<LayoutJob, CodeEditor>;
+
+    ctx.memory_mut(|mem| {
+        mem.caches
+            .cache::<HighlightCache>()
+            .get((code, language))
+    })
+}
+
 
 // impl Widget for CodeEditor {
 //     fn ui(&mut self, ui: &mut Ui) -> Response {

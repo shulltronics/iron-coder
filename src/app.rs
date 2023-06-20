dc869567a5a5ca13b081a460bb3a5dbc03021326
Carsten Thue-Bludworth <carstentb@protonmail.com>
2023-06-20 12:58:23 +0900

    moved debug build notice and acknowledgements to about window

--- src/app.rs
+++ src/app.rs
@@ -278,23 +278,6 @@ impl IronCoderApp {
                     }
                 }
             });
-
-            ui.with_layout(egui::Layout::bottom_up(egui::Align::Center), |ui| {
-                ui.horizontal(|ui| {
-                    egui::warn_if_debug_build(ui);
-                });
-                ui.horizontal(|ui| {
-                    ui.spacing_mut().item_spacing.x = 0.0;
-                    ui.label("powered by ");
-                    ui.hyperlink_to("egui", "https://github.com/emilk/egui");
-                    ui.label(" and ");
-                    ui.hyperlink_to(
-                        "eframe",
-                        "https://github.com/emilk/egui/tree/master/crates/eframe",
-                    );
-                    ui.label(".");
-                });
-            });
         });
 
         egui::Area::new("editor area").show(ctx, |_ui| {
@@ -394,6 +377,20 @@ impl IronCoderApp {
             );
             ui.label("Developed by Shulltronics");
             ui.hyperlink_to("Iron Coder on Github", "https://github.com/shulltronics/iron-coder");
+            ui.horizontal(|ui| {
+                egui::warn_if_debug_build(ui);
+            });
+            ui.horizontal(|ui| {
+                ui.spacing_mut().item_spacing.x = 0.0;
+                ui.label("powered by ");
+                ui.hyperlink_to("egui", "https://github.com/emilk/egui");
+                ui.label(" and ");
+                ui.hyperlink_to(
+                    "eframe",
+                    "https://github.com/emilk/egui/tree/master/crates/eframe",
+                );
+                ui.label(".");
+            });
         });
     }
 }

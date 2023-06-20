1d4a1aaed2aa4eb71530ac77284587aeea7d4093
Carsten Thue-Bludworth <carstentb@protonmail.com>
2023-06-20 17:33:29 +0900

    refactoring board module; added thin wrapper struct for different widget displays

--- src/app.rs
+++ src/app.rs
@@ -192,7 +192,7 @@ impl IronCoderApp {
                 for (i, b) in self.boards.clone().into_iter().enumerate() {
                     let col = i % num_cols;
                     // When a board is clicked, add it to the new project
-                    if columns[col].add(b).clicked() {
+                    if columns[col].add(board::BoardSelectorWidget(b)).clicked() {
                         board = Some(self.boards[i].clone());
                     }
                 }
--- src/board.rs
+++ src/board.rs
@@ -1,4 +1,4 @@
-use log::{info, warn};
+use log::warn;
 
 use std::path::{Path, PathBuf};
 use std::fs;
@@ -8,13 +8,7 @@ use std::cmp;
 
 use serde::{Serialize, Deserialize};
 
-use egui_extras::RetainedImage;
-
-use egui::{Ui, Response};
-use egui::widgets::Widget;
-use egui::{FontFamily, FontId};
-use egui::Color32;
-use egui::text::{TextFormat, LayoutJob};
+pub mod display;
 
 // this function reads the boards directory and returns a Vec in RAM
 // the boards directory is structured as:
@@ -87,6 +81,12 @@ pub struct Board {
     related_crates: Option<Vec<String>>,
 }
 
+// Define some thin wrappers around Board so we can display a Board with the
+//   Widget trait in multiple ways
+//   see https://doc.rust-lang.org/book/ch19-03-advanced-traits.html#using-the-newtype-pattern-to-implement-external-traits-on-external-types
+//   for the Newtype pattern
+pub struct BoardSelectorWidget(pub Board);
+
 impl fmt::Debug for Board {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "{}", self.name)
@@ -139,189 +139,4 @@ impl Board {
     pub fn get_name(&self) -> &str {
         self.name.as_str()
     }
-}
-
-// I might want to use this idea in the future:
-
-// pub struct BoardSelectorWidget;
-
-// impl BoardSelectorWidget {
-//     pub fn new() -> BoardSelectorWidget {
-//         return Self;
-//     }
-// }
-
-// impl Widget for BoardSelectorWidget {
-    
-//     fn ui(self, ui: &mut Ui) -> Response {
-//         let response = ui.allocate_response(egui::vec2(100.0, 200.0), egui::Sense::click());
-//         ui.painter().rect_stroke(response.rect, 0.0, (1.0, egui::Color32::WHITE));
-//         return response;
-//     }
-// }
-
-impl Widget for Board {
-    // How to display a board as a widget
-    fn ui(self, ui: &mut Ui) -> Response {
-        let response: egui::Response;
-        if let Some(color_image) = self.pic {
-            // Use a frame to display multiple widgets within our widget,
-            // with an inner margin
-            response = egui::Frame::none()
-            // .inner_margin(egui::Margin::same(10.0))
-            // .outer_margin(egui::Margin::same(3.0))
-            .show(ui, |ui| {
-                egui::CollapsingHeader::new(self.name.as_str())
-                .default_open(true)
-                .show(ui, |ui| {
-                    // center all text
-                    ui.with_layout(egui::Layout::top_down(egui::Align::Center), |ui| {
-                        // let label = egui::RichText::new(self.name).strong();
-                        ui.label(make_field_widget_text(
-                            "Board: ",
-                            ui.style().visuals.warn_fg_color,
-                            self.name.as_str(),
-                            ui.style().visuals.window_stroke.color,
-                        ));
-                        // ui.label(label);
-                        let retained_image = RetainedImage::from_color_image(
-                            "pic",
-                            color_image,
-                        );
-                        retained_image.show_max_size(ui, egui::vec2(150.0, 150.0));
-                    });
-                    ui.horizontal(|ui| {
-                        ui.label(make_field_widget_text(
-                            "Manufacturer: ",
-                            ui.style().visuals.warn_fg_color,
-                            self.manufacturer.as_str(),
-                            ui.style().visuals.window_stroke.color,
-                        ));
-                    // TODO -- make the manufacturer logos an app-wide resource
-                        // let p = Path::new("./assets/images/Adafruit_logo_small.png");
-                        // let image = image::io::Reader::open(p).unwrap().decode().unwrap();
-                        // let size = [image.width() as _, image.height() as _];
-                        // let image_buffer = image.to_rgba8();
-                        // let pixels = image_buffer.as_flat_samples();
-                        // let color_image = egui::ColorImage::from_rgba_unmultiplied(
-                        //     size,
-                        //     pixels.as_slice(),
-                        // );
-                        // let ri = egui_extras::RetainedImage::from_color_image("logo", color_image);
-                        // let image = egui::widgets::Image::new(
-                        //     ri.texture_id(ui.ctx()),
-                        //     egui::Vec2::new(47.0, 16.0)
-                        // ).tint(egui::Color32::GREEN);   // TODO: replace with a val from current colorscheme
-                        // ui.add(image);
-                    });
-                    ui.horizontal(|ui| {
-                        ui.label("Ecosystem: ");
-                        if let Some(standard) = self.standard {
-                            ui.label(standard.to_string());
-                        } else {
-                            ui.label("none");
-                        }
-                    });
-                    ui.horizontal(|ui| {
-                        ui.label("CPU: ");
-                        if let Some(cpu) = self.cpu {
-                            ui.label(cpu);
-                        } else {
-                            ui.label("unknown");
-                        }
-                    });
-                    ui.horizontal(|ui| {
-                        ui.label("RAM Amount (in kb): ");
-                        if let Some(ram) = self.ram {
-                            ui.label(ram.to_string());
-                        } else {
-                            ui.label("unknown");
-                        }
-                    });
-                    ui.horizontal(|ui| {
-                        ui.label("Flash Amount (in kb): ");
-                        if let Some(flash) = self.flash {
-                            ui.label(flash.to_string());
-                        } else {
-                            ui.label("unknown");
-                        }
-                    });
-                    ui.separator();
-                    // Show the examples
-                    ui.with_layout(egui::Layout::top_down(egui::Align::Center), |ui| {
-                        let label = egui::RichText::new("Examples").underline();
-                        ui.label(label);
-                    });
-                    for e in self.examples {
-                        ui.horizontal(|ui| {
-                            if ui.link(e.file_name().unwrap().to_str().unwrap()).clicked() {
-                                info!("TODO - open the example!")
-                            };
-                        });
-                    }
-
-                    ui.separator();
-                    // show the related crates
-                    ui.with_layout(egui::Layout::top_down(egui::Align::Center), |ui| {
-                        let label = egui::RichText::new("Related Crates").underline();
-                        ui.label(label);
-                    });
-                    if let Some(related_crates) = self.related_crates {
-                        for rc in related_crates.iter() {
-                            ui.horizontal(|ui| {
-                                if ui.link(rc).clicked() {
-                                    info!("TODO - deal with the related crate!")
-                                };
-                            });
-                        }
-                    }
-                });
-            }).response.interact(egui::Sense::click());
-
-            if ui.rect_contains_pointer(response.rect) {
-                // draw a bounding box
-                ui.painter().rect_stroke(response.rect, 0.0, (1.0, egui::Color32::WHITE));
-            }
-            
-            //another way of doing it; clicking works but scaling is off
-            // let th = ui.ctx().load_texture(
-            //     "pic",
-            //     color_image,
-            //     Default::default(),
-            // );
-            // let i = egui::Image::new(&th, egui::vec2(128.0, 128.0)).sense(egui::Sense::click());
-            // response = ui.add(i);
-        } else {
-            response = ui.allocate_response(egui::vec2(128.0, 128.0), egui::Sense::click());
-        }
-        return response;
-    }
-
-}
-
-// This function will construct a LayoutJob with a bold heading
-fn make_field_widget_text(heading: &str,
-                          hcolor: Color32,
-                          content: &str,
-                          ccolor: Color32) -> LayoutJob {
-    let mut job = LayoutJob::default();
-    job.append(
-        heading,
-        0.0,
-        TextFormat {
-            font_id: FontId::new(12.0, FontFamily::Name("MonospaceBold".into())),
-            color: hcolor,
-            ..Default::default()
-        },
-    );
-    job.append(
-        content,
-        0.0,
-        TextFormat {
-            font_id: FontId::new(12.0, FontFamily::Monospace),
-            color: ccolor,
-            ..Default::default()
-        },
-    );
-    return job;
 }

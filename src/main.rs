9879c66889d449343d34cd800990f5dca03c9423
Carsten Thue-Bludworth <carstentb@protonmail.com>
2023-06-27 20:57:06 +0900

    added necessary stuff for wasm, needs work to compile

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use tracing;
 
+// When compiling natively:
+#[cfg(not(target_arch = "wasm32"))]
 fn main() -> eframe::Result<()> {
     // Log to stdout (if you run with `RUST_LOG=debug`).
     tracing_subscriber::fmt().with_max_level(tracing::Level::DEBUG).init();
@@ -10,4 +12,24 @@ fn main() -> eframe::Result<()> {
         native_options,
         Box::new(|cc| Box::new(iron_coder::IronCoderApp::new(cc))),
     )
+}
+
+// When compiling to web using trunk:
+#[cfg(target_arch = "wasm32")]
+fn main() {
+    // Redirect `log` message to `console.log` and friends:
+    eframe::WebLogger::init(log::LevelFilter::Debug).ok();
+
+    let web_options = eframe::WebOptions::default();
+
+    wasm_bindgen_futures::spawn_local(async {
+        eframe::WebRunner::new()
+            .start(
+                "the_canvas_id", // hardcode it
+                web_options,
+                Box::new(|cc| Box::new(iron_coder::IronCoderApp::new(cc))),
+            )
+            .await
+            .expect("failed to start eframe");
+    });
 }

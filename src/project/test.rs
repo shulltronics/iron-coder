c5eecc0f7260649b2def87bff4f01659affb6d8d
Jacob <jacob.pyatt102@gmail.com>
2024-04-12 13:20:04 -0400

    Edited test to remove error messaage of missing repo

--- src/project/test.rs
+++ src/project/test.rs
@@ -4,7 +4,7 @@ mod project_tests {
 
     #[test]
     pub fn test_save_as() {
-        let mut project: Project = Project{name : "test_project".to_string(), location: None, system: Default::default(), code_editor: Default::default(), terminal_buffer: "".to_string(), receiver: None, current_view: Default::default(), known_boards: vec![] };
+        let mut project: Project = Project{name : "test_project".to_string(), location: None, system: Default::default(), code_editor: Default::default(), terminal_buffer: "".to_string(), receiver: None, current_view: Default::default(), known_boards: vec![], repo: Repository };
         project.save_as(true).expect("Project Failed to Save!");
     }
 }
